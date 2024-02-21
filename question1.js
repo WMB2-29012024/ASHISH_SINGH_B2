745c4565b9610afa704aa0381fcc92627661984a
ashishsingh2213 <ashishsingh2213@gmail.com>
2024-02-21 17:03:05 +0530

    W4_D2_Q1:write all the primitive data type

---
+++ question1.js
@@ -0,0 +1,6 @@
+// Create an array/list of names of all primitive data types in javascript.
+
+// List of all primitive data type
+
+const primitiveDataTypes = ["string", "number","boolean","null", "undefined", "symbol", "bigint"];
+console.log(primitiveDataTypes);
